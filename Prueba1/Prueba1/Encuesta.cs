c72f98dc6541f00f1515cc97150c14afec80540b
fernandofilipuzzi-utn <69309817+fernandofilipuzzi-utn@users.noreply.github.com>
2020-08-06 23:19:15 -0300

    explicacion
    subiendo cambios

---
+++ Prueba1/Prueba1/Encuesta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class Encuesta
+    {
+        public void Registrar(string nombre, int r1, int r2, int r3)
+        {
+
+        }
+    }
+}
--- Prueba1/Prueba1/Form1.cs
+++ Prueba1/Prueba1/Form1.cs
@@ -11,9 +11,40 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        Encuesta encuesta = new Encuesta();
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dialogo conf = new Dialogo();
+
+            //inicialización y limpieza
+            conf.checkBox1.Checked = false;
+
+            while (conf.ShowDialog() == DialogResult.OK)
+            {
+                
+
+                //procesa cargado/configurado
+              //  encuesta.Registrar(conf.c.Nombre,1,1,1);
+
+                if (conf.checkBox1.Checked == true)
+                    Text = "true";
+                else
+                    Text = "false";
+
+            }
+
+            conf.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
